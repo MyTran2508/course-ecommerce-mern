b0f16d8dbc50cdcd6a5fcff3c77378e928b21f9c
MyTran2508 <mytranchi2508@mail.com>
2023-12-15 13:25:23 +0700

    BE - add connection error

--- Backend/src/common/config/dbConnect.js
+++ Backend/src/common/config/dbConnect.js
@@ -3,8 +3,12 @@ const { default: mongoose } = require("mongoose");
 const dbConnect = () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = mongoose.connect(process.env.MONGODB_URL, () => {
+    const conn = mongoose.connect(process.env.MONGODB_URL);
+    mongoose.connection.on("connected", () => {
       console.log("Mongo connected");
+    });
+    mongoose.connection.on("error", (err) => {
+      console.error("Mongo connection error:", err);
     });
   } catch (error) {
     throw new Error("Database Error");
